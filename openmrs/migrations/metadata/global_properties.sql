f62a048a1f55639c30a83d9c0b0d5707cd36b021
Cosmin Ioan <cioan@pih.org>
2016-04-11 15:24:39 -0400

    add stored procedure for setting global properties

---
+++ openmrs/migrations/metadata/global_properties.sql
@@ -0,0 +1,5 @@
+-- This file should be used to set up GLOBAL_PROPERTY in the system
+
+-- GLOBAL_PROPERTY
+
+call ensure_global_property('concept.reasonForDeath', 'Reasons for Death', 'Concept id of the REASON FOR DEATH Concept Set');
