23ee06c905ba9894236487082be49bab89fd3215
Cosmin Ioan <cioan@pih.org>
2016-04-27 09:10:13 -0400

    SL-12, configure Bahmni Id to be used as the primary identifier type for Wellbody

--- openmrs/migrations/metadata/global_properties.sql
+++ openmrs/migrations/metadata/global_properties.sql
@@ -4,4 +4,4 @@
 
 call ensure_global_property('concept.reasonForDeath', 'Reasons for Death', 'Concept id of the REASON FOR DEATH Concept Set');
 
-call ensure_global_property('emr.primaryIdentifierType', '61CA5C17-0A60-4F4C-A3A3-61E17AAF4657', 'Primary identifier type for looking up patients, generating barcodes, etc');
+call ensure_global_property('emr.primaryIdentifierType', '7dfc1a64-e42f-11e5-8c3e-08002715d519', 'Primary identifier type for looking up patients, generating barcodes, etc');
--- openmrs/migrations/metadata/patient_identifiers.sql
+++ openmrs/migrations/metadata/patient_identifiers.sql
@@ -1,6 +1,6 @@
 -- This file is used to set up PATIENT_IDENTIFIER_TYPE in the system
 
 -- PATIENT_IDENTIFIER_TYPE
-call ensure_patient_identifier_type('Wellbody ID', 'New patient identifier type created for use at Wellbody Health Center', 1, '61CA5C17-0A60-4F4C-A3A3-61E17AAF4657');
+call ensure_patient_identifier_type('Bahmni Id', 'Patient identifier type for use at Wellbody Health Center', 1, '7dfc1a64-e42f-11e5-8c3e-08002715d519');
 
-call ensure_idgen_identifier_source('61CA5C17-0A60-4F4C-A3A3-61E17AAF4657', 'WBA', 'Local identifier generator for Wellbody' ,'1234567890', '100001', 'WBA', '', 9, 9, '88ce74ab-e0f9-11e5-be03-e82aea237783');
+call ensure_idgen_identifier_source('7dfc1a64-e42f-11e5-8c3e-08002715d519', 'WBA', 'Local identifier generator for Wellbody' ,'1234567890', '100001', 'WBA', '', 9, 9, '88ce74ab-e0f9-11e5-be03-e82aea237783');
