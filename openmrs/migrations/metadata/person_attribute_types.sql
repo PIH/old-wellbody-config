73d9b877d38d6a71e356086b73ec7b1fb0fea18b
Cosmin Ioan <cioan@pih.org>
2016-04-27 11:50:36 -0400

    SL-13, remove fields for names in local language from registration screen

---
+++ openmrs/migrations/metadata/person_attribute_types.sql
@@ -0,0 +1,8 @@
+-- This file is used to set up PERSON_ATTRIBUTE_TYPE in the system
+
+
+call retire_person_attribute_type('7e6db4ea-e42f-11e5-8c3e-08002715d519', 'remove from registration screen');
+call retire_person_attribute_type('7e6f78a4-e42f-11e5-8c3e-08002715d519', 'remove from registration screen');
+call retire_person_attribute_type('7e709f5b-e42f-11e5-8c3e-08002715d519', 'remove from registration screen');
+
+
